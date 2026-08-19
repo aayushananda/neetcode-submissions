563b9957d620c2249287b20c25551ea3fa4be8dd
Aayush Anand <133569111+aayushananda@users.noreply.github.com>
2026-08-19 22:14:07 +0530

    Add: duplicate-integer - submission-4

---
+++ Data Structures & Algorithms/duplicate-integer/submission-4.js
@@ -0,0 +1,12 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    hasDuplicate(nums) {
+    let set = new Set(nums);
+  if (set.size !== nums.length) return true;
+  else return false;
+
+    }
+}
