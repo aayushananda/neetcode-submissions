4cb2e389f039cdacf6cc4cfd47687928f8ac0c5c
Aayush Anand <133569111+aayushananda@users.noreply.github.com>
2026-08-20 19:09:16 +0530

    Add: is-anagram - submission-6

---
+++ Data Structures & Algorithms/is-anagram/submission-6.js
@@ -0,0 +1,35 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+      let map = new Map()
+
+      for(let i of s){
+        if(map.has(i)){
+          map.set(i,map.get(i)+1)
+        }
+        else{
+          map.set(i,1)
+        }
+      }
+
+      for(let i of t){
+        if(map.has(i)){
+          map.set(i, map.get(i)-1)
+        }
+        else{
+          return false
+        }
+      }
+
+      for(let [i,v] of map.entries()){
+        if(v!==0){
+          return false
+        }
+      }
+      return true
+    }
+}
